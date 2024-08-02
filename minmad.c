5c8b76fc3f97521ae5afacff3c5afe4d5f0a2e39
Ali Gholami Rudi <ali@rudi.ir>
2024-08-02 13:55:36 +0000

    minmad: fix warning for poll header

--- minmad.c
+++ minmad.c
@@ -7,12 +7,12 @@
  */
 #include <ctype.h>
 #include <fcntl.h>
+#include <poll.h>
 #include <pty.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <sys/poll.h>
 #include <termios.h>
 #include <unistd.h>
 #include <sys/soundcard.h>
