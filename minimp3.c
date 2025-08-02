fda62664f0f90dff54efb7a481b531a3de9be060
Ali Gholami Rudi <ali@rudi.ir>
2025-08-02 19:24:02 +0330

    minimp3: load a file completely, if its size is at most 16MB

--- minimp3.c
+++ minimp3.c
@@ -7,43 +7,69 @@
 #include "minimp3.h"
 #include "minmad.h"
 
+#define MMBUFLEN	(1 << 20)	/* buffer length */
+#define MMBUFMIN	(1 << 14)	/* minimum amount of buffer for decoding */
+#define MMBUFFULL	(1 << 24)	/* load files smaller than this size */
+
+#define MIN(a, b)	((a) < (b) ? (a) : (b))
+
 static mp3dec_t dec;
-static int mm_fd;
-static char *mm_buf;
-static long mm_len;
-static long mm_pos;
+static int mm_fd;	/* mp3 file fd */
+static long mm_len;	/* mm_fd size */
+static long mm_pos;	/* decoding position */
+
+static char *buf;
+static long buf_sz;
+static long buf_off;
+static long buf_len;
 
 int mm_init(char *path)
 {
 	struct stat st;
-	int pos = 0;
-	int nr;
 	mp3dec_init(&dec);
 	if ((mm_fd = open(path, O_RDONLY)) < 0)
 		return 1;
-	if (fstat(mm_fd, &st) < 0 || st.st_size == 0)
-		return 1;
-	mm_len = st.st_size;
-	if (!(mm_buf = malloc(mm_len))) {
+	if (fstat(mm_fd, &st) < 0 || st.st_size == 0) {
 		close(mm_fd);
 		return 1;
 	}
-	while ((nr = read(mm_fd, mm_buf + pos, mm_len - pos)) > 0)
-		pos += nr;
-	close(mm_fd);
+	mm_len = st.st_size;
+	buf_sz = mm_len <= MMBUFFULL ? mm_len : MMBUFLEN;
+	if (!(buf = malloc(buf_sz))) {
+		close(mm_fd);
+		return 1;
+	}
 	return 0;
 }
 
 int mm_done(void)
 {
-	free(mm_buf);
+	free(buf);
+	close(mm_fd);
 	return 0;
 }
 
 int mm_decode(void *dst, long *dst_len, int *bytes, int *rate, int *ch, int *bits)
 {
 	mp3dec_frame_info_t info = {0};
-	int n = mp3dec_decode_frame(&dec, (void *) mm_buf + mm_pos, mm_len - mm_pos, (void *) dst, &info);
+	int n;
+	/* fill buf if necessary */
+	if (mm_pos < buf_off || MIN(mm_pos + MMBUFMIN, mm_len) > buf_off + buf_len) {
+		int nr;
+		if (mm_pos > buf_off && mm_pos < buf_off + buf_len) {
+			memmove(buf, buf + (mm_pos - buf_off), buf_off + buf_len - mm_pos);
+			buf_len = (buf_off + buf_len) - mm_pos;
+			buf_off = mm_pos - buf_len;
+		} else {
+			buf_off = mm_pos;
+			buf_len = 0;
+		}
+		lseek(mm_fd, buf_off + buf_len, 0);
+		while ((nr = read(mm_fd, buf + buf_len, buf_sz - buf_len)) > 0)
+			buf_len += nr;
+	}
+	n = mp3dec_decode_frame(&dec, (void *) buf + (mm_pos - buf_off),
+		(buf_off + buf_len) - mm_pos, (void *) dst, &info);
 	*dst_len = n * info.channels * 2;
 	*rate = info.hz;
 	*ch = info.channels;
